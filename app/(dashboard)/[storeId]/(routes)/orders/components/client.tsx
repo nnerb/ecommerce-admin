5b0437ea17c956338bcc74a6731ae49e51b0e1fe
Brenn <brennaldwin.santiago>
2023-07-23 20:24:12 +0800

    final

---
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+interface OrderClientProps {
+    data: OrderColumn[]
+}
+
+import Heading from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+
+import { OrderColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
+
+
+export const OrderClient: React.FC<OrderClientProps> = ({
+    data
+}) => {
+
+    return ( 
+        <>
+            <Heading 
+                title={`Orders (${data.length})`}
+                description="Manage orders for your store"
+            />
+            <Separator />
+            <DataTable searchKey="products" columns={columns} data={data} />
+        </>
+     );
+}
+ 
